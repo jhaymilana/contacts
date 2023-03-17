11b2b0dcc1bb23a70061b64ec1844c78ca128a77
Jayvee Milana <jmmilana12@gmail.com>
2023-03-17 10:14:23 -0500

    contact add js

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,3 +8,25 @@ Contacts
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 'use strict';
+
+let storage = document.querySelector('.contact-storage');
+let add = document.querySelector('.add');
+let output = document.querySelector('.output p');
+
+function createContact() {
+  let newContact = document.createElement('div');
+
+  newContact.className = `contact-info`;
+  storage.appendChild(newContact);
+}
+
+add.addEventListener('click', function() {
+  if (storage.childElementCount <= 19) {
+    createContact();
+    output.innerHTML = 'Contact saved.'
+//  unitArray.push(``);
+  } else {
+    output.innerHTML = 'Please delete 1 contact.'
+  }
+});
+
