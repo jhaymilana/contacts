5db02cdf82ed348efb4df73441eb883166dc31a6
Jayvee Milana <jmmilana12@gmail.com>
2023-03-17 11:09:23 -0500

    input text linked

--- assets/script/index.js
+++ assets/script/index.js
@@ -10,6 +10,7 @@ Contacts
 'use strict';
 
 let storage = document.querySelector('.contact-storage');
+let input = document.querySelector('.contact-input');
 let add = document.querySelector('.add');
 let output = document.querySelector('.output p');
 
@@ -18,11 +19,14 @@ function createContact() {
   let contactName = document.createElement('p');
   let contactCity = document.createElement('p');
   let contactEmail = document.createElement('p');
+  let inputArr = input.value.split(',');
 
   newContact.className = `contact-info`;
-  contactName.innerHTML = 'Name:';
-  contactCity.innerHTML = 'City:';
-  contactEmail.innerHTML = 'Email:';
+  
+  contactName.innerHTML = `Name: ${inputArr[0]}`;
+  contactCity.innerHTML = `City: ${inputArr[1]}`;
+  contactEmail.innerHTML = `Email: ${inputArr[2]}`;
+
   storage.appendChild(newContact);
   newContact.appendChild(contactName);
   newContact.appendChild(contactCity);
@@ -33,7 +37,6 @@ add.addEventListener('click', function() {
   if (storage.childElementCount <= 19) {
     createContact();
     output.innerHTML = 'Contact saved.'
-//  unitArray.push(``);
   } else {
     output.innerHTML = 'Please delete 1 contact.'
   }
