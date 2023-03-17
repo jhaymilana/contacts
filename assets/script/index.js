b802e5d0545ee9f626e8baf0a6eeb3106737eacd
Jayvee Milana <jmmilana12@gmail.com>
2023-03-17 10:38:19 -0500

    info text added

--- assets/script/index.js
+++ assets/script/index.js
@@ -15,9 +15,18 @@ let output = document.querySelector('.output p');
 
 function createContact() {
   let newContact = document.createElement('div');
+  let contactName = document.createElement('p');
+  let contactCity = document.createElement('p');
+  let contactEmail = document.createElement('p');
 
   newContact.className = `contact-info`;
+  contactName.innerHTML = 'Name:';
+  contactCity.innerHTML = 'City:';
+  contactEmail.innerHTML = 'Email:';
   storage.appendChild(newContact);
+  newContact.appendChild(contactName);
+  newContact.appendChild(contactCity);
+  newContact.appendChild(contactEmail);
 }
 
 add.addEventListener('click', function() {
